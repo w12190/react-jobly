39512208a66a25133dfa3a8d3af915dde1001627
Cam T. Bui <ncbui@users.noreply.github.com>
2020-09-01 17:43:44 -0700

    add useEffect to handle API calls and update state. render company cards

--- src/CompanyList.js
+++ src/CompanyList.js
@@ -1,4 +1,8 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+
+import JoblyApi from './api';
+import SearchForm from './SearchForm';
+import CompanyCard from './CompanyCard'
 
 /** Displays a list of companies.
  *
@@ -6,11 +10,44 @@ import React, {useState} from 'react'
  */
 
  function CompanyList(){
+  console.log("<CompanyList />")
   const [companyList, setCompanyList] = useState([])
+  console.log("companyList", companyList);
+
+  useEffect(
+    function handleApiRequests(){
+      async function fetchData() {
+        console.log("fetchData() for <CompanyList />")
+        try {
+          const companies = await JoblyApi.getAllCompanies();
+          // setCompanyList((oldCompanyList) => [...oldCompanyList, companies]);
+          setCompanyList(companies);
+        } catch (err) {
+          alert(err);
+        }
+      }
+      fetchData()
+    },[]
+   )
+  // TODO: add state holding searchterm to useEffect dependencies
+  // TODO: add <SearchForm />
+
+   const cardsInfo = companyList.map(c => {
+      return {
+        handle: c.handle,
+        name: c.name,
+        description: c.description,
+        logoUrl: c.logoUrl
+      }
+   }) 
+   console.log("companyList mapped for simple info", cardsInfo);
 
   return (
     <div>
-    
+      <SearchForm />
+      {cardsInfo.map(c=> {
+        return <CompanyCard data={c}/>
+        })}
     </div>
   )
  }
