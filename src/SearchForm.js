92910fbdc62198be06feab0000a555323f553864
Cam T. Bui <ncbui@users.noreply.github.com>
2020-09-01 17:44:13 -0700

    add scaffolding for component

---
+++ src/SearchForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+/**
+ *
+ * Design: www.tinyurl.com/y3ree3wj
+ */
+
+function SearchForm() {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default SearchForm
